7c1714edb09b76824b0a4ac620bfc636b3c38efd
shaoytsh <shyttsyj@126.com>
2019-08-05 15:31:56 +0800

    sf 下载

---
+++ js/index.js
@@ -0,0 +1,57 @@
+$(function () {
+    $('#layerslider').layerSlider({
+        skinsPath: 'layerslider/skins/',
+        animateFirstLayer: true,
+        navPrevNext: true,
+        navStartStop: false,
+        navButtons: false,
+        autoPlayVideos: false,
+        skin: 'minimal'
+    });
+    setHits();
+    var interval_output = setInterval("setHits()", 60000);
+    try {
+        var hiddenProperty = 'hidden' in document ? 'hidden' :
+            'webkitHidden' in document ? 'webkitHidden' :
+            'mozHidden' in document ? 'mozHidden' :
+            null;
+        var visibilityChangeEvent = hiddenProperty.replace(/hidden/i, 'visibilitychange');
+        var onVisibilityChange = function () {
+            if (!document[hiddenProperty]) {
+                setHits();
+                interval_output = setInterval("setHits()", 60000);
+            } else {
+                clearInterval(interval_output);
+            }
+        }
+        document.addEventListener(visibilityChangeEvent, onVisibilityChange);
+    } catch (e) {
+        setInterval("setHits()", 60000);
+    }
+});
+$(window).load(function () {
+    changeFooter();
+    window.onresize = function () {
+        var res;
+        if (res) { clearTimeout(res) }
+        res = setTimeout(changeFooter(), 20);
+    };
+});
+
+function setHits() {
+    $.post('api/getlist', {
+        _rid: Math.random()
+    },
+    function (str) {
+        $('#dynamicInfo').html(str);
+    });
+}
+function changeFooter() {
+    $('.bottom').css('min-height', 0);
+    var rangeIndex = document.documentElement.clientHeight - $('#footer').offset().top - $('#footer').height();
+    if (rangeIndex > 0) {
+        $('.bottom').css('min-height', rangeIndex);
+    } else {
+        $('.bottom').css('min-height', 0);
+    }
+}
